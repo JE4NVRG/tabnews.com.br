ec7aa08f52e94af33dc2151013517460e15ebb37
Jean Carlos Vargas <106420077+JE4NVRG@users.noreply.github.com>
2024-11-26 00:32:49 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews.com.br
+
 Projeto TabNews: Aprendizado e colaboração em desenvolvimento web. Baseado no curso.dev, este repositório é um espaço aberto para implementações e contribuições da comunidade. Vamos aprender e construir juntos!
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>O sucesso começa com o primeiro passo, ou neste caso, o primeiro arquivo. </h1>
+  return (
+    <h1>
+      O sucesso começa com o primeiro passo, ou neste caso, o primeiro arquivo.{" "}
+    </h1>
+  );
 }
 
 export default Home;
